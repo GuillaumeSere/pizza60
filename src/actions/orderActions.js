82c90fcd31312bf12928e5718ccb7227a80e617d
Guillaume Sere <guillaumesere@hotmail.fr>
2025-05-20 16:28:19 +0200

    modif server

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_PUBLIC_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_PUBLIC_API_URL || 'http://localhost:8000';
 
 export const placeOrder = (token, subtotal) => async (dispatch, getState) => {
 
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { resetCart } from './cartActions';
 
-const API_URL = process.env.REACT_PUBLIC_API_URL || 'http://localhost:8000';
+const API_URL = process.env.REACT_APP_PUBLIC_API_URL || 'http://localhost:8000';
 
 export const registerUser = (user) => async dispatch => {
 
